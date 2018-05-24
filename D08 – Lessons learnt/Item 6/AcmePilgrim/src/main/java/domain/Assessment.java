31b902187cbdcc7124b7af8a658f85e6b168b80c
Julio <julcarfer@gmail.com>
2018-05-24 21:36:36 +0200

    Subida de proyecto

---
+++ D08 – Lessons learnt/Item 6/AcmePilgrim/src/main/java/domain/Assessment.java
@@ -0,0 +1,55 @@
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Embeddable
+@Access(AccessType.PROPERTY)
+public class Assessment{
+
+	// Attributes -------------------------------------------------------------
+	private Date creationMoment;
+	private Integer ratingLenght;
+	private Integer ratingDifficulty;
+	private String commentAssessment;
+	
+	// Getters&Setters ----------------------------------------------------
+	@Past
+	@DateTimeFormat(pattern="dd/MM/yyyy HH:mm")
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getCreationMoment() {
+		return creationMoment;
+	}
+	public void setCreationMoment(Date creationMoment) {
+		this.creationMoment = creationMoment;
+	}
+	@Range(min = 0, max = 10)
+	public Integer getRatingLenght() {
+		return ratingLenght;
+	}
+	public void setRatingLenght(Integer ratingLenght) {
+		this.ratingLenght = ratingLenght;
+	}
+	@Range(min = 0, max = 10)
+	public Integer getRatingDifficulty() {
+		return ratingDifficulty;
+	}
+	public void setRatingDifficulty(Integer ratingDifficulty) {
+		this.ratingDifficulty = ratingDifficulty;
+	}
+	public String getCommentAssessment() {
+		return commentAssessment;
+	}
+	public void setCommentAssessment(String commentAssessment) {
+		this.commentAssessment = commentAssessment;
+	}
+}
